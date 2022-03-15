db1834c7ab846db546b62b40e2a60ac28fa6346f
KirubelEnyew <kirastarboys@gmail.com>
2022-03-15 22:05:15 +0300

    feat - added validation and registration functions

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,30 +1,47 @@
 const mongoose = require('mongoose')
+const joi = require('joi')
 
 const UserSchema = mongoose.Schema({
-    full_name: {
+    fullName: {
         type: String,
         required: true
     },
     email: {
         type: String,
+        unique: true,
         required: true
     },
     password: {
         type: String,
-        minLength: 6
+        minLength: 6,
+        required: true
     },
     phoneNumber: {
         type: String,
+        unique: true,
         minLength: 9,
-        maxLength: 9
+        maxLength: 9,
+        required: true
     }
 })
 
 const UserModel = mongoose.model('users', UserSchema)
 
-const addUser = async (newUser) => {
-    await UserModel(newUser).save().then((results)=>results).catch(()=>null)
+const addUser = (newUser) =>
+    UserModel(newUser).save()
+        .then(() => { return { result: 'Registration successfull', status: true } })
+        .catch((error) => { return { result: error, status: false } })
+
+const validateUser = (newUser) => {
+    const validationSchema = joi.object({
+        fullName: joi.string().required().min(2),
+        phoneNumber: joi.string().pattern(new RegExp('^[0-9]*$')).length(9).required(),
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    })
+    return validationSchema.validate(newUser)
 }
 
 exports.User = UserModel
 exports.addUser = addUser
+exports.validateUser = validateUser
