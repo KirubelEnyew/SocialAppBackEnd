0792d41fcc9323571c48bc9c552dc2d9da65860d
KirubelEnyew <kirastarboys@gmail.com>
2022-03-16 21:26:20 +0300

    chore : put exposed DB credential into env file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const app = require('./app')
 const http = require('http')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
 // connection goes here
 
 const server = http.createServer(app)
-const dbUri = 'mongodb+srv://LeulHBT:1234test@cluster0.5kpjp.mongodb.net/SampleAppDB?retryWrites=true&w=majority'
+const dbUri = `mongodb+srv://LeulHBT:${process.env.DB_PASSWORD}@cluster0.5kpjp.mongodb.net/SampleAppDB?retryWrites=true&w=majority`
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
