60db1f04a35d2e2c353ee5308ab5c7a103b5b318
Kirubel Enyew <kirubeleneyew@gmail.com>
2022-03-16 17:04:38 +0300

    feat - added email checking on sign up

--- Routes/user.js
+++ Routes/user.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const route = express.Router()
 const bcrypt = require('bcryptjs')
-const { addUser, validateUser } = require('../Models/UserModel')
+const { addUser, validateUser, existingEmail } = require('../Models/UserModel')
 
 route.post('/signUp', async (req, res) => {
     const { error } = validateUser(req.body)
+    // the 2 ifs below should have status codes for bad data
     if (error) {
         return res.status(500).json({ message: error })
+    }
+    const emailExists = await existingEmail(req.body.email)
+    if (emailExists) {
+        return res.status(500).json({ message: 'Email already exists', exists: true })
     }
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
@@ -26,6 +31,7 @@ route.post('/signUp', async (req, res) => {
 
 route.post('/login', (req, res) => {
     // insert token generation here
+    // fetch from db with email and then bcrypt compare with req.body
 })
 
 module.exports = route
