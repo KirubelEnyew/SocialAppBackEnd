4ce61a36514981e87c6a13b328465583413a1d9e
KirubelEnyew <kirastarboys@gmail.com>
2022-03-17 23:08:59 +0300

    feat - added account mgmt route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const user = require('./Routes/user')
+const account = require('./Routes/account')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 
 // routes go here
-app.use('/user',user)
+app.use('/user', user)
+app.use('/account', account)
 
 module.exports = app
