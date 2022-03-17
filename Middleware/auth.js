bc180147496247a50a23f790a9266ce8577f4208
KirubelEnyew <kirastarboys@gmail.com>
2022-03-17 23:08:34 +0300

    feat - added authentication middleware

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,1 +1,15 @@
 const jwt = require('jsonwebtoken')
+const authenticateUser = (req, res, next) => {
+    const token = req.header('Authorization')
+    if (!token) {
+        return res.status(400).json({ message: 'Authorization missing' })
+    }
+    jwt.verify(token, process.env.TOKEN_KEY, (error, decoded) => {
+        if (decoded) {
+            req.payload = decoded
+            return next()
+        }
+        return res.status(400).json({ message: error })
+    })
+}
+module.exports = authenticateUser
