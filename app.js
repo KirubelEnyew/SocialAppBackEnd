92fe360f56e200aece7fdc33173a829d979e8b17
KirubelEnyew <kirastarboys@gmail.com>
2022-03-18 21:09:06 +0300

    fix - added req limit to allow base 64 req body

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const account = require('./Routes/account')
 const express = require('express')
 const app = express()
 
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }))
 
 // routes go here
 app.use('/user', user)
