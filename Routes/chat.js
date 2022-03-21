3f1565044018f290ea9448fd1d52d9915664b415
KirubelEnyew <kirastarboys@gmail.com>
2022-03-21 21:10:26 +0300

    feat - added message sending feature

--- Routes/chat.js
+++ Routes/chat.js
@@ -1,9 +1,40 @@
 const route = require('express').Router()
 const { ChatModel } = require('../Models/ChatModel')
+const auth = require('../Middleware/auth')
 
-route.post('/sendMessage', auth, (req, res) => {
+route.post('/sendMessage', auth, async (req, res) => {
     // try $elemMathc in another scenario 
-    // use findOne instead of exists?
-    const chatExists = ChatModel.exists({ participants: [req.payload.id, req.body.reciever] })
-    console.log(chatExists)
+    // would ,$or:[req.body.reciever,req.payload.id] be required in the query below 
+    const chatExists = await ChatModel.findOne({ participants: { $all: [req.payload.id, req.body.reciever] } })
+    if (chatExists) {
+        ChatModel.updateOne(chatExists,
+            {
+                $push:
+                {
+                    messages:
+                    {
+                        sender: req.payload.id,
+                        message: req.body.message,
+                        date: new Date().toString()
+                    }
+                }
+            })
+            .then(() => res.status(200).json({ message: 'Message sent' }))
+            .catch(() => res.status(500).json({ message: 'Unable to send message' }))
+    } else {
+        ChatModel({
+            participants: [req.payload.id, req.body.reciever],
+            messages: [{
+                sender: req.payload.id,
+                message: req.body.message,
+                date: new Date().toString()
+            }]
+        }).save()
+            .then(() => res.status(200).json({ message: 'Message sent' }))
+            .catch(() => res.status(500).json({ message: 'Unable to send message' }))
+    }
 })
+
+// add edit and delete(also for whole chat) for specific messages
+
+module.exports = route
