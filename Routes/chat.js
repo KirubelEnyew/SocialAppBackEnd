8053d47aaa7976576bb099fda22d70e32db46b94
KirubelEnyew <kirastarboys@gmail.com>
2022-03-22 20:48:08 +0300

    feat - added chat history and message removal

--- Routes/chat.js
+++ Routes/chat.js
@@ -4,9 +4,9 @@ const auth = require('../Middleware/auth')
 
 route.post('/sendMessage', auth, async (req, res) => {
     // try $elemMathc in another scenario 
-    // would ,$or:[req.body.reciever,req.payload.id] be required in the query below 
-
+    // would ,$or:[req.body.reciever,req.payload.id] be required in the query below, UPDATE : $all does the inteded action 
     // remember to add socket for sending the message as well
+
     const chatExists = await ChatModel.findOne({ participants: { $all: [req.payload.id, req.body.reciever] } })
     if (chatExists) {
         ChatModel.updateOne(chatExists,
@@ -38,5 +38,30 @@ route.post('/sendMessage', auth, async (req, res) => {
 })
 
 // add edit and delete(also for whole chat) for specific messages
+route.post('/editMessage', auth, (req, res) => {
+    ChatModel.findOneAndUpdate(
+        { 'messages._id': req.body.messageId },
+        {
+            $set: {
+                'messages.$.message': req.body.editedMessage,
+                edited: true
+            }
+        }
+    )
+        .then(() => res.status(200).send({ message: 'Message edited' }))
+        .catch(() => res.status(400).send({ message: 'Unable to edit message' }))
+})
+
+route.post('/deleteMessage', auth, (req, res) => {
+    ChatModel.findOneAndDelete({ 'messages._id': req.body.messageId })
+        .then(() => res.status(200).send({ message: 'Message deleted' }))
+        .catch(() => res.status(500).send({ message: 'Unable to delete message' }))
+})
+
+route.post('/deleteChat', auth, (req, res) => {
+    ChatModel.findOneAndDelete({ _id: req.body.chatId })
+        .then(() => res.status(200).send({ message: 'Chat history deleted' }))
+        .catch(() => res.status(500).send({ message: 'Unable to delete chat history' }))
+})
 
 module.exports = route
